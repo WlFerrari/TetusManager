be26d29b9ef91d3ccab601ac1e17eb8a35566ba6
Ferrari <98551353+WlFerrari@users.noreply.github.com>
2026-08-21 00:22:28 -0300

    fix: restringir dados da empresa ao administrador

--- backend/routes/empresa.routes.js
+++ backend/routes/empresa.routes.js
@@ -2,11 +2,7 @@ const router = require('express').Router()
 const { authMiddleware, requirePerm } = require('../middleware/auth')
 const EmpresaController = require('../controllers/EmpresaController')
 
-/** GET /api/empresa */
-router.get('/', authMiddleware, (req, res, next) => EmpresaController.show(req, res, next))
-
-/** PUT /api/empresa */
+router.get('/', authMiddleware, requirePerm('verEmpresa'), (req, res, next) => EmpresaController.show(req, res, next))
 router.put('/', authMiddleware, requirePerm('verEmpresa'), (req, res, next) => EmpresaController.update(req, res, next))
 
 module.exports = router
-
